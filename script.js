46a43c5e29ee974d53457e7802c3a917a46d4248
Sandy Chu <sandy.chu.40@gmail.com>
2023-02-23 01:18:40 -0800

    Changes to script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const http = require('k6/http');
 const { check, sleep } = require('k6');
 
-const TOKEN = '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';
 export const options = {
   stages: [
     { duration: '10s', target: 1100 },
@@ -9,9 +8,6 @@ export const options = {
   ],
 };
 export default function () {
-  const params = {
-    headers: { Authorization: TOKEN }
-  };
   // const newestReviews = http.get('http://localhost:3000/reviews/newest/1');
   const metaReviews = http.get('http://localhost:3000/reviews/meta/123');
   check(metaReviews, { 'status was 200': (r) => r.status === 200 });
